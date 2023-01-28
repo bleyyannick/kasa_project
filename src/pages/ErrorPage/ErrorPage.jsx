cb9d7f48574b26f6fb945a6e187b6b006a0a7507
Yannick Bley <bleyyannickpro@gmail.com>
2023-01-28 09:30:39 +0100

    building error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,16 @@
-
+import { Link } from 'react-router-dom';
+import styles from './ErrorPage.module.css'; 
 
 
 const ErrorPage = () => {
   return (
-  <>
-     FAUTE !!!
-  </>
+  <div className={styles.container} >
+    <h1>404</h1>
+    <p>Oups! La page que vous demandez n'existe pas</p>
+     <Link to='/'>
+       Retourner sur la page d'accueil
+     </Link>
+  </div>
   )
 }
 
